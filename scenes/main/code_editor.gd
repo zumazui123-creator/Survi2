01c37e21917fcf3577c26dc852badb78c6bcd390
zumazui123-creator <zumazui123@gmail.com>
2025-11-14 21:26:52 +0100

    s

--- scenes/main/code_editor.gd
+++ scenes/main/code_editor.gd
@@ -1,28 +1,33 @@
 extends Node2D
-#
-#@onready var code_edit = $CodeEdit
-#@onready var item_list = $HBoxContainer/VBoxContainer3/ItemList
-#@onready var player =  "res://scenes/character/player.gd"
-#
-#func _ready() -> void:
-	#code_edit.code_completion_enabled = true
-	#code_edit.code_completion_prefixes = ["links","ob"]
-#
-#func _process(delta: float) -> void:
-	#pass
-#
-#func _on_button_pressed_links() -> void:
-	##code_edit.get_text_with_cursor_char()
-	#
-	#code_edit.insert_text_at_caret("links\n")
-	##code_edit.text += "oben\n"
-#
-#
-#func _on_button_pressed_oben() -> void:
-	#code_edit.insert_text_at_caret("oben\n")
-#
-#
-#func _on_item_list_item_clicked(index: int, at_position: Vector2, mouse_button_index: int) -> void:
-	#var item_text = item_list.get_item_text(index)
-	#code_edit.insert_text_at_caret(item_text+"\n")
-	#
+
+@onready var code_edit = $CodeEdit
+@onready var item_list = $HBoxContainer/VBoxContainer3/ItemList
+@onready var player =  "res://scenes/character/player.gd"
+
+func _ready() -> void:
+	var syntax_highlighter = CodeSyntaxHighlighter.new()
+	syntax_highlighter.add_keyword_color("wiederhole", Color.YELLOW)
+	syntax_highlighter.add_keyword_color("ende", Color.YELLOW)
+	code_edit.syntax_highlighter = syntax_highlighter
+	
+	code_edit.code_completion_enabled = true
+	code_edit.code_completion_prefixes = ["links","ob"]
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_button_pressed_links() -> void:
+	#code_edit.get_text_with_cursor_char()
+	
+	code_edit.insert_text_at_caret("links\n")
+	#code_edit.text += "oben\n"
+
+
+func _on_button_pressed_oben() -> void:
+	code_edit.insert_text_at_caret("oben\n")
+
+
+func _on_item_list_item_clicked(index: int, at_position: Vector2, mouse_button_index: int) -> void:
+	var item_text = item_list.get_item_text(index)
+	code_edit.insert_text_at_caret(item_text+"\n")
+	
