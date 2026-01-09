4add76ba82d16f42ca19284178e725985f5c292b
zumazui123-creator <zumazui123@gmail.com>
2026-01-09 12:47:59 +0100

    ^=

--- scenes/ui/minimap/PointsDraw.gd
+++ scenes/ui/minimap/PointsDraw.gd
@@ -9,7 +9,7 @@ const PLAYER_COLOR := Color(1.0, 0.0, 0.0)   # Red color for player
 func _ready():
 	var main_root = get_tree().root 
 	var map = get_node("/root/Map")
-	map.get_node("TileMap")
+	#map.get_node("TileMap")
 	tilemap = get_node("../../../../../Map/TileMap")
 
 func _process(_delta):
